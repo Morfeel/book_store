08c8e0e05a4e9128d90742f6216f29ab7eada09a
Geoffrey <morfeel@gmail.com>
2015-02-01 10:44:22 +1300

    add model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  root 'main#index'
+  
+  #get 'main/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  match ':controller(/:action(/:id))', :via => :get
 end
